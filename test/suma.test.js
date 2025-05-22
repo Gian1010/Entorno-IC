3b45d4616cf5f0d5e65c231a03af7d7d4ae82fd2
Gianfranco <gianfranconiveiro@hotmail.com>
2025-05-22 11:54:37 -0300

    prueba 22-5

---
+++ test/suma.test.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const suma = require('../src/suma');
+
+describe('Función suma', () => {
+    it('debería retornar 10 cuando se suman 6 + 4', () => {
+        const resultado = suma(6, 5);
+    assert.strictEqual(resultado, 10);
+    });
+});
+
+//it lo uso para definir una prueba individual
+//describe me agrupa un conjunto de pruebas
+//assert.strictequal verifica que mi resultado de la suma sea = 10
