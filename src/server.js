8f1c0d503c470bbd4ada36f51ceee407e750fbd0
Gianfranco <gianfranconiveiro@hotmail.com>
2025-05-23 18:31:37 -0300

    pruebas trello

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     const resultado = suma(5, 5);
-    res.send(`El resultado de la suma es: ${resultado} hola`);
+    res.send(`El resultado de la suma es: ${resultado}`);
 });
 
 app.listen(port, () => {
