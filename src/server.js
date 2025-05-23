f5b24448efa19fe078a645875c230bc8a2b58a7a
Gianfranco <gianfranconiveiro@hotmail.com>
2025-05-23 13:08:59 -0300

    pruebas 23-05

--- src/server.js
+++ src/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const suma = require('./suma');
 
 const app = express();
-const port = 3000;
+// Puerto dinámico para entornos como Render
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     const resultado = suma(5, 5);
@@ -12,6 +13,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
 
 //utilizo nodemon para que mi server me actualice en tiempo real los cambios
 //npm run dev para abrirlo su conf esta en package.json
